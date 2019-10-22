0386b71d9a3d0e0442fd50d4580fe6962a8e9d7d
Ivan Dmitriev <Realdeathstroke@yandex.ru>
2019-10-23 01:01:11 +0300

    add test

---
+++ test_items.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_purchase_button(browser):
+    browser.get(link)
+    # Согласно заданию запускайте код так: pytest --language=es test_items.py
+    # Проверяющим: Если хотите проверить смену языка - раскоментируйте следующую строчку
+    # time.sleep(30)
+    try:
+        browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+        assert True, "Нет кнопки 'добавить в корзину' "
+    except NoSuchElementException:
+        assert False, "Нет кнопки 'добавить в корзину' "
